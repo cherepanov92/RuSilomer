f33769f4f8c335bdcaccf4efa8bb029fa5bb7bbc
Max <acad.teh@gamil.com>
2020-09-20 02:58:04 +0500

    Кнопка Аккаунта

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import MenuBurger from '../Menu/MenuBurger';
 import Menu from '../Menu/Menu';
 import Logo  from "../Logo/Logo";
 import Calc_Icon  from "../Calc_Icon/Calc_Icon";
+import Account_Icon from '../Account/Account_Icon';
 import {connect} from 'react-redux';
 import cl from 'classnames';
 
@@ -11,6 +12,7 @@ const Header = ({navShow}) => {
         <div className="header">
             <Calc_Icon clname={cl({"header__calc--hidden": navShow}, "header__calc")}/>
             <Logo clname={cl({"header__logo--width": navShow}, "header__logo")}/>
+            <Account_Icon clname="header__account"/>
             <MenuBurger clname="header__burger"/>
             <Menu />
         </div>
