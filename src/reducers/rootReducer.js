e0a1d33c5a3dd7e61f1d1fe335d22051c5dd765d
Cherepanov Artyom <cherepanov92@gmail.com>
2026-01-18 02:18:45 +0500

    выпилил лишнее

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,21 +2,18 @@ import {combineReducers} from 'redux'
 import {HYDRATE} from 'next-redux-wrapper'
 import navReducer from './navReducer'
 import modalReducer from './modalReducer'
-import cityReducer from './cityReduser'
 
 const rootReducer = combineReducers({
   nav: navReducer,
   modal: modalReducer,
-  city: cityReducer,
 })
 
 const reducer = (state, action) => {
   if (action.type === HYDRATE) {
-    const nextState = {
+    return {
       ...state,
       ...action.payload,
     }
-    return nextState
   } else {
     return rootReducer(state, action)
   }
